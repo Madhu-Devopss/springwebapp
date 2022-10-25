bc7e3e23b81f7bbcf5d201cdaf09d1d7d4bbffcf
Madhu-Devopss <madhusekhar.powerbi@gmail.com>
2022-10-25 12:35:39 +0530

    hello world app

---
+++ helloworld/src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		
+		return "hello world...";
+		
+	}
+
+}
